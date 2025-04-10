65f9e0717539ee4dedfc56139e965898e39e033c
Stephan Geulette <s.geulette@imio.be>
2025-04-10 15:43:01 +0200

    Forgotten module

--- src/collective/dms/basecontent/browser/views.py
+++ src/collective/dms/basecontent/browser/views.py
@@ -1,23 +1,29 @@
 from collective.documentviewer.browser.views import DocumentViewerView
+from plone.base.utils import human_readable_size
 from plone.dexterity.browser.edit import DefaultEditForm
 from plone.dexterity.browser.view import DefaultView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.component import getMultiAdapter
 
-
 import json
 
-# TODO : MIGRATION-PLONE6
 
 class VersionViewerView(DocumentViewerView):
-    pass
+    def get_obj_size(self, context):
+        size = context.get_size()
+        return human_readable_size(size)
+
+    def get_content_type(self, context):
+        if not context.file:
+            return ""
+        return context.file.contentType
 
 
 class JSONVersionViewerView(DocumentViewerView):
     def index(self):
         self.request.response.setHeader("Content-Type", "application/json")
         return json.dumps(self.dv_data())
-# END TODO
+
 
 class DmsDocumentView(DefaultView):
     def update(self):
