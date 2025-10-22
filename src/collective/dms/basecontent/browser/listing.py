49d6a50a601446086662cce86a4b02cf51c7c8bf
Chris Adam <chris.adam@imio.be>
2025-10-22 15:55:53 +0200

    Hide iconfied columns if it's not used

--- src/collective/dms/basecontent/browser/listing.py
+++ src/collective/dms/basecontent/browser/listing.py
@@ -2,6 +2,10 @@
 from collective.dms.basecontent import _
 from collective.dms.basecontent.browser import column
 from collective.dms.basecontent.browser.table import Table
+from collective.iconifiedcategory.browser.tabview import ApprovedColumn
+from collective.iconifiedcategory.browser.tabview import PrintColumn
+from collective.iconifiedcategory.browser.tabview import SignedColumn
+from collective.iconifiedcategory.utils import get_category_object
 from Products.CMFCore.utils import getToolByName
 from zope.cachedescriptors.property import CachedProperty
 from zope.i18n import translate
@@ -23,8 +27,28 @@ class BaseTable(Table):
 
 
 class VersionsTable(BaseTable):
-
     cssClasses = {"table": "listing nosort dv iconified-listing"}
+
+    def _get_content_category_group(self, context):
+        return get_category_object(context, context.content_category).get_category_group()
+
+    def setUpColumns(self):
+        columns = super(VersionsTable, self).setUpColumns()
+
+        final_columns = []
+        for col in columns:
+            if isinstance(col, PrintColumn) and not any((self._get_content_category_group(v.getObject()).to_be_printed_activated for v in self.values)):
+                continue
+            elif isinstance(col, SignedColumn) and not any((self._get_content_category_group(v.getObject()).signed_activated for v in self.values)):
+                continue
+            elif isinstance(col, ApprovedColumn) and not any((self._get_content_category_group(v.getObject()).approved_activated for v in self.values)):
+                continue
+            final_columns.append(col)
+        return final_columns
+
+    def updateColumns(self):
+        super(VersionsTable, self).updateColumns()
+        self.columns = self.columns[:-3] + list(reversed(self.columns[-3:]))
 
 
 class DmsAppendixTable(VersionsTable):
