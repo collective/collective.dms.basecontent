4919ce641bda7b3dd3a7b976b9302c6c64285999
Stephan Geulette <s.geulette@imio.be>
2025-11-20 12:58:50 +0100

    Made buildout and tests running

--- src/collective/dms/basecontent/testing.py
+++ src/collective/dms/basecontent/testing.py
@@ -1,7 +1,8 @@
 # -*- coding: utf8 -*-
-
+from plone import api
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import PloneWithPackageLayer
+from zope.globalrequest import setLocal
 
 import collective.dms.basecontent
 
@@ -13,10 +14,24 @@ DMS = PloneWithPackageLayer(
     name="DMS",
 )
 
-DMS_TESTS_PROFILE = PloneWithPackageLayer(
-    bases=(DMS,),
+# DMS_TESTS_PROFILE = PloneWithPackageLayer(
+#     bases=(DMS,),
+# )
+
+
+class DmsLayer(PloneWithPackageLayer):
+
+    def setUpPloneSite(self, portal):
+        portal.portal_registration.addMember(id="siteadmin", password="SiteAdm!n0")
+        api.group.add_user(groupname="Administrators", username="siteadmin")
+        setLocal("request", portal.REQUEST)
+        super(DmsLayer, self).setUpPloneSite(portal)
+
+
+DMS_TESTS_PROFILE = DmsLayer(
     zcml_filename="testing.zcml",
     zcml_package=collective.dms.basecontent,
+    # additional_z2_products=("Products.PythonScripts", "imio.dashboard", "imio.dms.mail", "Products.PasswordStrength"),
     gs_profile_id="collective.dms.basecontent:testing",
     name="DMS_TESTS_PROFILE",
 )
